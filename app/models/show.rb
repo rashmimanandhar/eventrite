4eb5cc1d8ba531292852ebfa02b585d4382b16bd
Rashmi <rashmi.manandhar7@gmail.com>
2021-08-05 10:38:37 -0400

    feat: add validation to shows form

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,7 @@
 class Show < ApplicationRecord
+  validates :artist, presence: true
+  validates :venue, presence: true
+  validates :event_on, presence: true
   belongs_to :artist
   belongs_to :venue
 end
